0b5c6a3eeb620550c62549b2f3d1bcef44dde27c
AlanRacciatti <alanracciatti1220@gmail.com>
2023-03-19 12:32:01 -0300

    feat: create vault manager contract

---
+++ solidity/contracts/VaultManager.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.4 <0.9.0;
+
+import {IERC20} from 'isolmate/interfaces/tokens/IERC20.sol';
+import {IERC4626} from 'interfaces/tokens/IERC4626.sol';
+
+contract VaultManager {
+  function deposit(address token, address vault) external payable {}
+}
