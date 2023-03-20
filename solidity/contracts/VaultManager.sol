1a75d82ebf116f0b1ee162b53625be30533ec4b7
AlanRacciatti <alanracciatti1220@gmail.com>
2023-03-20 17:54:11 -0300

    refactor: addresses set on constructor

--- solidity/contracts/VaultManager.sol
+++ solidity/contracts/VaultManager.sol
@@ -13,25 +13,31 @@ import {CurveHelper} from 'libraries/CurveHelper.sol';
 import {WETH9Helper} from 'libraries/WETH9Helper.sol';
 
 contract VaultManager {
+  address public immutable WETH_ADDRESS;
+  address public immutable SWAP_ROUTER_ADDRESS;
+
   IWETH9 public immutable WETH;
   ISwapRouter public immutable SWAP_ROUTER;
 
   constructor(address _weth9, address _swapRouter) {
+    WETH_ADDRESS = _weth9;
+    SWAP_ROUTER_ADDRESS = _swapRouter;
+
     WETH = IWETH9(_weth9);
     SWAP_ROUTER = ISwapRouter(_swapRouter);
   }
 
   function deposit(uint256 _amount, address _token, address _vault, uint24 _poolFee) external returns (uint256 _shares) {
-    WETH9Helper.pullEthFromSender(_amount, address(WETH));
-    uint256 _amountOut = SwapHelper.swapEthForToken(_token, _amount, _poolFee, address(WETH), address(SWAP_ROUTER));
+    WETH9Helper.pullEthFromSender(_amount, WETH_ADDRESS);
+    uint256 _amountOut = SwapHelper.swapEthForToken(_token, _amount, _poolFee, WETH_ADDRESS, SWAP_ROUTER_ADDRESS);
 
     IERC20(_token).approve(_vault, _amountOut);
     _shares = IERC4626(_vault).deposit();
   }
 
-  function deposit(uint256 _amount, address _token, address _pool, address _vault) external returns (uint256 _shares) {
-    WETH9Helper.pullEthFromSender(_amount, address(WETH));
-    uint256 _amountOut = CurveHelper.addCurveLiquidity(_pool, _token, address(WETH), _amount);
+  function deposit(uint256 _amount, address _token, address _vault, address _pool) external returns (uint256 _shares) {
+    WETH9Helper.pullEthFromSender(_amount, WETH_ADDRESS);
+    uint256 _amountOut = CurveHelper.addCurveLiquidity(_pool, _token, WETH_ADDRESS, _amount);
 
     IERC20(_token).approve(_vault, _amountOut);
     _shares = IERC4626(_vault).deposit();
